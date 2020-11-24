6cb1e1b22cbc6683816644dcd00f6f5b7bafb320
pixal-lab <pablo.ahumada1@mail.udp.cl>
2020-11-24 13:00:45 -0300

    all tables added

--- database/database.sql
+++ database/database.sql
@@ -1,21 +1,71 @@
 CREATE DATABASE tienda;
 
-CREATE table empleados(
-    id Serial primary key,
-    name varchar(100),
-    email TEXT
+create table Productos(
+    ID_producto serial primary key,
+    Nombre text,
+    Stock int,
+    Precio int,
+    Categoria text,
+    Descripcion text
 );
 
-Insert Into empleados (name, email) Values
-    ('pablo', 'pablo@udp.cl'),
-    ('pp', 'pp@mail.udp.cl');
+create table Personal(
+    Nombre text,
+    Rut int primary key,
+    Puesto text,
+    Fecha_empleo date,
+    Salario int
+);
 
-CREATE table Productos(
-    id Serial primary key,
-    nombre TEXT,
-    stock integer,
-    precio int,
-    categoria TEXT,
-    descripcion TEXT
+create table Cliente(
+    Nombre text,
+    Rut int primary key,
+    Direccion text,
+    Correo text,
+    Inscrito boolean
+);
 
+create table Venta(
+    ID_venta serial primary key,
+    Rut_cliente int,
+    Rut_trabajador int,
+    Fecha date,
+    Precio int,
+    constraint FK_cli
+        foreign key (Rut_cliente)
+        references Cliente(Rut),
+    constraint FK_per
+        foreign key (Rut_trabajador)
+        references Personal(Rut)
+);
+
+create table Detalle_de_venta(
+    ID_venta int,
+    ID_producto int,
+    Cantidad int,
+    constraint FK_ven
+        foreign key (ID_venta)
+        references Venta(ID_venta),
+    constraint FK_prod
+        foreign key (ID_producto)
+        references Productos(ID_producto)
+);
+
+create table Compra(
+    ID_compra serial primary key,
+    Precio_total int,
+    Fecha_compra date
+);
+
+create table Detalle_de_compra(
+    ID_compra int,
+    ID_producto int,
+    Cantidad int,
+    Precio_unitario int,
+    constraint FK_comp
+        foreign key (ID_compra)
+        references Compra(ID_compra),
+    constraint FK_prod
+        foreign key (ID_producto)
+        references Productos(ID_producto)
 );
