5afe308b4ab74c5f000a4d23afdee9155968326d
zeltic <benjaminbasauri@gmail.com>
2020-11-29 18:52:31 -0300

    Add files via upload

--- Project/src/app/shared/services/auth.service.ts
+++ Project/src/app/shared/services/auth.service.ts
@@ -8,21 +8,10 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
-	authUrl= "http://190.163.174.21:5000/users";
-	APIUrl= "http://190.163.174.21:5000";
+	readonly APIUrl= "http://190.163.174.21:5000";
 
 	constructor(private http:HttpClient) { }
-
-	login(model: any){
-		return this.http.post(this.authUrl, model).pipe(
-			map((response: any)=>{
-				const user = response;
-				if(user.result.succeeded){
-					localStorage.setItem('token', user.token);
-				}
-			})
-		)
-	}
+	
 	register_client(model: any) {
 		return this.http.post(this.APIUrl + '/addClient', model);
 	}
@@ -31,5 +20,8 @@ export class AuthService {
 	}
 	get_client(model: any):Observable<any> {
 		return this.http.get(this.APIUrl + '/getCliente?rut=' + model.rut).pipe(map((res:any)=>{return res;}));
+	}
+	get_employer(model: any):Observable<any> {
+		return this.http.get(this.APIUrl + '/getEmployer?rut=' + model.rut).pipe(map((res:any)=>{return res;}));
 	}
 }
---
+++ Project/src/app/shared/services/getall.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class GetallService {
+
+	readonly APIUrl= "http://190.163.174.21:5000";
+
+	constructor(private http:HttpClient) { }
+
+	getAllClientes():Observable<any[]>{
+		return this.http.get<any>(this.APIUrl+'/getAllClientes');
+	}
+	getAllPersonal():Observable<any[]>{
+		return this.http.get<any>(this.APIUrl+'/getAllPersonal');
+	}
+	getAllProd():Observable<any[]>{
+		return this.http.get<any>(this.APIUrl+'/getAllProd');
+	}
+	getAllVentas():Observable<any[]>{
+		return this.http.get<any>(this.APIUrl+'/getAllVentas');
+	}
+}
