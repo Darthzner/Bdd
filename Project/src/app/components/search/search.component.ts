0e17002323948dadb135c2631f566e149b8d4e87
Darkelposho <56275990+Darkelposho@users.noreply.github.com>
2020-12-10 16:11:40 -0300

    Add files via upload

--- Project/src/app/components/search/search.component.ts
+++ Project/src/app/components/search/search.component.ts
@@ -9,32 +9,48 @@ import { CartService } from '../../services/cart.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
   public searchJson: any;
-  public find: any;
-  public clave: any;
-  public id: any;
+  public product_item: any;
+  public product_list: any[] = [];
   constructor(private searchservice: SearchService, private cartservice: CartService) { 
     this.searchservice.getSearch()
     .subscribe( respuesta => {
-      //console.log("respuesta", respuesta)
       this.searchJson = respuesta;
-
-      //localStorage.clear()
+      for(let i =0; i<this.searchJson.length; i++){
+        this.product_list.push(0);
+      }
     })
   }
 
-  Data($e){
-    //this.cartservice.addCart($e);
-    if(!localStorage.getItem($e.id_producto)){
-      localStorage.setItem($e.id_producto,$e.precio);
+  Data($e,x:number){
+    if(!localStorage.getItem($e.nombre)){
+      const obj = $e;
+      obj.cantidad = x
+      this.product_item = JSON.stringify(obj);
+      localStorage.setItem($e.nombre,this.product_item);
     }else{
-      
+      console.log("hola")
+      let value =JSON.parse(localStorage.getItem($e.nombre))
+      console.log(value.cantidad)
+      value.cantidad = value.cantidad + x
+      console.log(value.cantidad)
+      this.product_item = JSON.stringify(value);
+      localStorage.setItem($e.nombre,this.product_item);
     }
     
   }
   
+  handleMinus(index:number) {
+    if(this.product_list[index]>0){
+      this.product_list[index]--;
+    }
+  }
+  handlePlus(index:number) {
+    this.product_list[index]++; 
+  }
+
   ngOnInit(): void {
+    
   }
 
 }
