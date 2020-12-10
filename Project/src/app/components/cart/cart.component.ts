8bcacdc95a09982381ffc8deb0f2c0aea4654e78
Darkelposho <56275990+Darkelposho@users.noreply.github.com>
2020-12-10 16:10:55 -0300

    Add files via upload

--- Project/src/app/components/cart/cart.component.ts
+++ Project/src/app/components/cart/cart.component.ts
@@ -8,20 +8,40 @@ import { CartService } from '../../services/cart.service';
 })
 
 export class CartComponent implements OnInit {
+  public searchJson: any;
   public CartItem = new Object();
-  constructor( private cartservice: CartService) {}
+  constructor( private cartservice: CartService) {
+    for(let x=0;x<localStorage.length;x++){
+      let keyy = localStorage.key(x)
+      let value = JSON.parse(localStorage.getItem(keyy)) 
+      this.CartItem[keyy] = value;
+      console.log(this.CartItem[keyy])
+      //this.CartItem = this.cartservice.getCartItems();
+    }
+  }
  /*clearCartItems() {
     this.CartItem = {}
     this.cartservice.clearCart
   }*/
+  
+  Totalprice(id:number){
+    let value = JSON.parse(localStorage.getItem(id)) 
+    let total = value.precio*value.cantidad;
+    return total
+  }
+
+  Buy(){
+    
+  }
+
+  RemoveItem(id:number){
+    localStorage.removeItem(id);
+    delete this.CartItem[id];
+    console.log(localStorage);
+  }
+
   ngOnInit(): void {
-    for(let x=0;x<localStorage.length;x++){
-      let keyy = localStorage.key(x)
-      let value = localStorage.getItem(keyy)
-      this.CartItem[keyy] = value;
-      //this.CartItem = this.cartservice.getCartItems();
-    }
-    console.log(this.CartItem)
+    
   }
 }
   
